35d98222722b0c2ff77873c53ae82022a7e5ebbd
beingsksharma <30936556+beingsksharma@users.noreply.github.com>
2022-08-11 18:50:29 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page1 from './Page1';
-// import Page2 from './Page2';
-// import Page3 from './Page3';
-// import Page4 from './Page4';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
 
 function App() {
   return (
-    <Page1 />
-  // <Router>
-  //   <Routes>
-  //     <Route path="/" element={<Page1 />} />
-  //     <Route path="/second" element={<Page2 />} />
-  //     <Route path="/third" element={<Page3 />} />
-  //     <Route path="/fourth" element={<Page4 />} />
-  //   </Routes>
-  // </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Page1 />} />
+      <Route path="/second" element={<Page2 />} />
+      <Route path="/third" element={<Page3 />} />
+      <Route path="/fourth" element={<Page4 />} />
+    </Routes>
+  </Router>
   );
 }
 
